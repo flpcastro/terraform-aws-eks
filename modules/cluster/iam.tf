dd2a69025f785cb45974c7710cf0019974cc2ae7
Felipe Curto <flpc.dev@gmail.com>
2025-05-28 02:59:53 -0300

    feat: create cluster module

---
+++ modules/cluster/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.project_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
